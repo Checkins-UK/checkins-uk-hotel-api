f4fe0c4e062dba9e869309d9c201c25f580a129c
Amila T Kumarasekara <84329334+amila-t-kumarasekara@users.noreply.github.com>
2024-03-10 22:23:49 +0530

    install compression

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 const port = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  // Enable gzip compression
+  app.use(compression());
   await app.listen(port);
 }
 bootstrap();
